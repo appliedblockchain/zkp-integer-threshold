b25c6cc7099b22a943fa22e36114974795411255
makevoid <makevoid@gmail.com>
2019-05-15 00:48:47 +0100

    full flow

--- zkp-hash-range-example.js
+++ zkp-hash-range-example.js
@@ -11,6 +11,62 @@ const ZKP = require('./lib/zkp')
 const KYC = require('./lib/kyc')
 
 
+// ---------
+
+
+// CLI
+
+// Enter your Age
+
+// Age: 21
+
+// Welcome in my motorbike shop, you bought a motorbike for 2k, prove that you're 18.
+
+// > [Buy]
+
+// > [Auth with Nuggets]
+
+// > [Download Nuggets]
+
+// I open my phone and visit http://localhost:3000 / https://abdevs.eu.ngrok.io / https://mkvd.eu.ngrok.io
+
+// > [Auth with Nuggets]
+
+// I push Auth with Nuggets
+
+// I see I'm logged in
+
+// I see a button "New proof request", "Generate Send Proof" (Website details: "motorbike shop")
+
+// > [Generate and Send Proof]
+
+// Contacting KYC server for secret
+
+// Secret Received
+
+// Generating proof...
+
+// Generated proof 12341232323123123
+
+// Are you sure you want to send the following proof?
+
+// user: {
+//   id: 123,
+//   username: "Jhon Doe (#123)",
+// },
+// timestamp: 1557849798,
+// encryptedAge: "012030120301230012031203010203012300123012030"
+
+// > [Yes/No]
+
+// I can now buy a motorbike.
+
+
+// ---------
+
+
+// KYC Provider
+
 const requiredAge = 18
 const age = 21
 
