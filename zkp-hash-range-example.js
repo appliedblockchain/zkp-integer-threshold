3e74c0b440eadd0074b09370bc22e088e8b58936
makevoid <makevoid@gmail.com>
2019-05-19 02:14:40 +0100

    use zkp/zkp-integer-treshold.js, remove libs

--- zkp-hash-range-example.js
+++ zkp-hash-range-example.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const {
-  encryptAge,
-  proveAge,
-  verifyAge,
-} = require('./lib/zkp-range-proofs-age')
+  encryptIntegerThreshold,
+  genIntegerThresholdProof,
+  verifyIntegerThreshold,
+} = require('./zkp/zkp-integer-threshold')
 
 const { NullFieldError } = require('./lib/errors.js')
 const inquirer = require('inquirer')
@@ -30,21 +30,19 @@ const main = async () => {
   age = new Number(age.age)
 
   await promptNext('Welcome in my motorbike shop, you are buying a motorbike for 2k...', inquirer)
-  await wait(500)
+  await wait(200)
 
   console.log("You'll have to prove that you\'re over 18 to complete the purchase.\n")
-  await wait(500)
+  await wait(200)
 
-  await promptNext('> Select Bike: "GS500 600cc"', inquirer)
+  await promptNext('> Select Bike: "GS200 600cc"', inquirer)
 
   await promptNext('> Button: [Buy]', inquirer)
 
   await promptNext('> Button: [Auth with Nuggets]', inquirer)
 
   console.log("I open the Nuggets App...\n")
-  await wait(500)
-
-  // I open my phone and visit http://localhost:3000 / https://abdevs.eu.ngrok.io / https://mkvd.eu.ngrok.io
+  await wait(200)
 
   await promptNext('I see I\'m logged in, I see a notification "New proof request"', inquirer)
 
@@ -53,52 +51,52 @@ const main = async () => {
   await promptNext('> Button: [Generate and Send Proof]', inquirer)
 
   console.log("...")
-  await wait(500)
+  await wait(200)
 
   console.log("Contacting KYC server for secret...\n")
-  await wait(800)
+  await wait(200)
 
 
   // ran by Service:
 
   console.log("Secret Received\n")
-  await wait(500)
+  await wait(200)
 
   const seed = genSeed()
   console.log(`Seed (secret): ${formatHex(seed)}\n`)
-  await wait(500)
+  await wait(200)
 
   console.log("Encrypting age...")
-  await wait(500)
+  await wait(200)
 
-  const encryptedAge = encryptAge(age, seed)
+  const encryptedAge = encryptIntegerThreshold(age, seed)
 
   console.log(`Age Encrypted: ${formatHex(encryptedAge)}\n`)
-  await wait(500)
+  await wait(200)
 
   console.log("Encrypted age Sent alongside Digital Signature and timestamp from provider\n")
-  await wait(500)
+  await wait(200)
 
 
   // ran by User:
 
   console.log("Generating proof...")
-  await wait(500)
+  await wait(200)
 
   const requiredAge = 18 // age treshold
 
-  const proof = proveAge(age, requiredAge, seed)
+  const proof = genIntegerThresholdProof(age, requiredAge, seed)
 
   console.log(`Proof: ${formatHex(proof)}`)
-  await wait(500)
+  await wait(200)
 
   console.log("Proof generated and sent!\n")
-  await wait(500)
+  await wait(200)
 
 
   // ran by Service:
 
-  const verification = verifyAge(proof, requiredAge)
+  const verification = verifyIntegerThreshold(requiredAge, proof)
 
   console.log(`Age verification challenge result: ${formatHex(verification)} \n\n`)
 
